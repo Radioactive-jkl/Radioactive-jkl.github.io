b402bc305b58ad2e8687e47be565d2cb3b306e40
Radioactive-jkl <2934217127@qq.com>
2023-08-12 17:47:33 +0800

    Site updated: 2023-08-12 17:47:33

--- js/white.js
+++ js/white.js
@@ -1,134 +1,113 @@
-function isActive(pathname) {
-    let rootPath = window.location.pathname
-    let path = pathname + "/"
-    return pathname === rootPath || path === rootPath;
-}
+
+//下面这一段全部来自于hexo-theme-nexmoe的部分源码，感谢大佬，指路: https://github.com/theme-nexmoe/hexo-theme-nexmoe
+const getRealPath = (pathname, desc = false) => {
+    if (!pathname) {
+      pathname = window.location.pathname;
+    }
+    let names = pathname.split("/");
+    if (desc === false) {
+      for (let i = names.length - 1; i >= 0; --i) {
+        let name = names[i].trim();
+        if (name.length > 0 && name !== "/" && name !== "index.html") {
+          return name;
+        }
+      }
+    } else {
+      for (let i = 0; i < names.length; ++i) {
+        let name = names[i].trim();
+        if (name.length > 0 && name !== "/" && name !== "index.html") {
+          return name;
+        }
+      }
+    }
+    return "/";
+  };
 
 let links = document.querySelectorAll(".nav-link");
+let rootRealPath = getRealPath(window.location.pathname, true);
 for (let link of links) {
-    let linkPath = link.getAttribute("href");
-
-    if (isActive(linkPath)) {
-        link.className = "active-link nav-link";
-    }
+  let linkPath = link.getAttribute("href");
+  if (linkPath && getRealPath(linkPath, true) === rootRealPath) {
+    link.className = "active-link nav-link";
+  }
 }
-
+// mobile-nav-link
 let mlinks = document.querySelectorAll(".mobile-nav-link");
+let mrootRealPath = getRealPath(window.location.pathname, true);
 for (let mlink of mlinks) {
-    let mlinkPath = mlink.getAttribute("href");
-    if (isActive(mlinkPath)) {
-        mlink.className = "moibile-active-link mobile-nav-link";
-    }
+  let mlinkPath = mlink.getAttribute("href");
+  if (mlinkPath && getRealPath(mlinkPath, true) === mrootRealPath) {
+    mlink.className = "moibile-active-link mobile-nav-link";
+  }
 }
 
-// 设置收起的menu点击的效果
-var mmenu = document.getElementById("mobile-menu");
-var mmain = document.getElementById("mobile-main");
-var mclose = document.getElementById("mobile-close");
+//设置收起的menu点击的效果
+var mmenu =document.getElementById("mobile-menu");
+var mmain=document.getElementById("mobile-main");
+var mclose=document.getElementById("mobile-close");
 
-mmenu.onclick = function () {
-    mmain.style.display = "block";
+mmenu.onclick=function(){
+  mmain.style.display="block";
 }
-mclose.onclick = function () {
-    mmain.style.display = "none";
+mclose.onclick=function(){
+  mmain.style.display="none";
 }
 
-// 切换颜色
-// let change = document.getElementById("theme-change-btn")
-// let changeList = [
-//     {
-//         text: "白昼",
-//         code: "day"
-//     },
-//     {
-//         text: "午夜",
-//         code: "night"
-//     },
-//     {
-//         text: "饼干",
-//         code: "cookie"
-//     },
-//     {
-//         text: "森林",
-//         code: "forest"
-//     }
-// ]
-// var currentIndex = 0
-// change.onclick = function (){
-//     console.log("you click change theme btn and currentIndex = " + currentIndex)
-//     currentIndex = (currentIndex + 1) % changeList.length;
-//     this.innerHTML = changeList[currentIndex].text;
-//     document.body.className = changeList[currentIndex].code;
-//     // 将状态存入缓存中
-//     localStorage.setItem('hexo-white-theme-mode', changeList[currentIndex].code);
-// }
-//
-// let beginCode = localStorage.getItem('hexo-white-theme-mode') || "cookie" // day是默认
-// let tmpElement = changeList.filter(item => item.code === beginCode)[0]
-// change.innerHTML = tmpElement.text
-// currentIndex = changeList.indexOf(tmpElement)
-// document.body.className = tmpElement.code
-// localStorage.setItem('hexo-white-theme-mode', tmpElement.code);
 
 ///////////////////////////////////////////////////////////////////////////////////
 
 //设置sort的子目录隐藏显示效果
-if (document.getElementById("sort")) {
-    var sort = document.getElementById("sort");
-    var sortdiv = document.getElementById("sort-div");
+if(document.getElementById("sort")){
+var sort=document.getElementById("sort");
+var sortdiv=document.getElementById("sort-div");
 
-    sort.onmouseover = function () {
-        sortdiv.style.display = "block";
-    }
-    sort.onmouseout = function () {
-        sortdiv.onmouseover = function () {
-            console.log("wuhu");
-        }
-        sortdiv.onmouseout = function () {
-            sortdiv.style.display = "none";
-        }
-    }
+sort.onmouseover=function(){
+  sortdiv.style.display="block";
+}
+sort.onmouseout=function(){
+sortdiv.onmouseover=function(){
+  console.log("wuhu");
+}
+sortdiv.onmouseout=function(){
+    sortdiv.style.display="none";
+}
+}
 }
 
-// 图片懒加载
-var imgs = document.querySelectorAll('img.lazyload-img');
+//图片懒加载
+var imgs = document.querySelectorAll('img');
 var imgdivs = document.querySelectorAll('.lazyload-img-span');
 
 
-// 用来判断bound.top<=clientHeight的函数，返回一个bool值
+//用来判断bound.top<=clientHeight的函数，返回一个bool值
 function isIn(el) {
     var bound = el.getBoundingClientRect();
     var clientHeight = window.innerHeight;
     return bound.top <= clientHeight;
-}
-
-// 检查图片是否在可视区内，如果在，则加载
+} 
+//检查图片是否在可视区内，如果在，则加载
 function check() {
-    for (let p = 0; p < imgs.length; p++) {
-        if (isIn(imgs[p])) {
-            loadImg(imgs[p]);
-            imgs[p].onload = function () {
-                let y = Number(p);
-                changeClass(y);
-            }
+  for(let p=0 ; p<imgs.length;p++){
+    if(isIn(imgs[p])){
+        loadImg(imgs[p]);
+        imgs[p].onload = function(){
+            let y=Number(p);
+            changeClass(y);
         }
     }
 }
-
-function changeClass(num) {
-    console.log("changeClass(num) num = "+num)
-    let tempse = Number(num);
-    imgdivs[tempse].className = "lazyload-img-span img-masks";
 }
-
+function changeClass(num){
+  let tempse=Number(num);
+  imgdivs[tempse].className = "lazyload-img-span img-masks";
+}
 function loadImg(el) {
-    console.log("loadImg el.dataset.src = "+el.dataset.src)
-    if (!el.src) {
-        el.src = el.dataset.src;
+    if(!el.src){
+        var source = el.dataset.src;
+        el.src = source;
     }
 }
 
-// onscroll()在滚动条滚动的时候触发
-window.onload = window.onscroll = function () {
+window.onload = window.onscroll = function () { //onscroll()在滚动条滚动的时候触发
     check();
 }
